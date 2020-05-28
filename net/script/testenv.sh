2d01354a307115388d1efa689b7c6164be46489a
MilkFather <31627231+MilkFather@users.noreply.github.com>
2020-05-28 16:57:50 +0800

    Update testenv.sh

--- net/script/testenv.sh
+++ net/script/testenv.sh
@@ -3,7 +3,7 @@
 Test_Pose_Environment() {
     if [ ! -d "poseenv" ]; then
         echo "Virtual environment for Pose-Transfer module not found. Creating one";
-        python3 -m venv "poseenv"
+        virtualenv "poseenv"
         echo "Setting up virtual environment"
         source ./poseenv/bin/activate
         python3 -m pip install -r "Pose-Transfer/requirements.txt" -i "https://mirrors.aliyun.com/pypi/simple/"
@@ -14,7 +14,7 @@ Test_Pose_Environment() {
 Test_Dress_Environment() {
     if [ ! -d "dressenv" ]; then
         echo "Virtual environment for DG-Net module not found. Creating one"
-        python3 -m venv "dressenv"
+        virtualenv "dressenv"
         echo "Setting up virtual environment"
         source ./dressenv/bin/activate
         python3 -m pip install -r "DG-Net/requirements.txt" -i "https://mirrors.aliyun.com/pypi/simple/"
