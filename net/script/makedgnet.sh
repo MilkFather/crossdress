e2640329b27e6b941e45f58dc6fecd7608a6a619
MilkFather <31627231+MilkFather@users.noreply.github.com>
2020-05-30 03:38:24 +0800

    Update makedgnet.sh

--- net/script/makedgnet.sh
+++ net/script/makedgnet.sh
@@ -4,6 +4,6 @@ Test_Dress_Environment
 
 source dressenv/bin/activate
 cd DG-Net
-python3 "DG-Net/make.py" --input_folder "../$1" --make_phase $2
+python3 "make.py" --input_folder "../$1" --make_phase $2
 cd ..
 deactivate
