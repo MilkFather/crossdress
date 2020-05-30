f79fd4142d0379d3ee97e94e0fd46de136f7b846
MilkFather <31627231+MilkFather@users.noreply.github.com>
2020-05-31 01:15:24 +0800

    Update testdataset.py

--- net/testdataset.py
+++ net/testdataset.py
@@ -3,4 +3,4 @@ import os
 
 if __name__ == "__main__":
     opts = TestOptions().parse_args()
-    os.system("./script/testdataset.sh \"{0}\" {1}".format(os.path.join(opts.dataroot, ".."), '+'.join(opts.use_data)))
+    os.system("./script/testdataset.sh \"{0}\" {1}".format(os.path.join("..", opts.dataroot, ".."), '+'.join(opts.use_data)))
