c8e8d50c928bdc13ecc8db55b627966998ed927a
MilkFather <31627231+MilkFather@users.noreply.github.com>
2020-09-07 13:36:51 +0800

    update path

--- net/testdataset.py
+++ net/testdataset.py
@@ -4,6 +4,6 @@ import os
 if __name__ == "__main__":
     opts = TestOptions().parse_args()
     if not opts.use_resnet:
-        os.system("./script/testdataset.sh \"{0}\" {1}".format(os.path.join("..", opts.dataroot, ".."), '+'.join(opts.use_data)))
+        os.system("./script/testdataset.sh \"{0}\" {1}".format(os.path.join("..", opts.dataroot), '+'.join(opts.use_data)))
     else:
-        os.system("./script/validatedataset.sh \"{0}\" {1}".format(os.path.join("..", opts.dataroot, ".."), '+'.join(opts.use_data)))
+        os.system("./script/validatedataset.sh \"{0}\" {1}".format(os.path.join("..", opts.dataroot), '+'.join(opts.use_data)))
