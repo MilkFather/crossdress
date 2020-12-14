49388b07a950689ce39c2fc5f4abec0d48f97473
MilkFather <31627231+MilkFather@users.noreply.github.com>
2020-12-14 16:23:22 +0800

    test submit

--- net/testdataset.py
+++ net/testdataset.py
@@ -3,7 +3,10 @@ import os
 
 if __name__ == "__main__":
     opts = TestOptions().parse_args()
-    #if not opts.use_resnet:
-    os.system("./script/testdataset.sh \"{0}\" {1} {2}".format(os.path.join("..", opts.dataroot), '+'.join(opts.use_data), str(opts.sample_size)))
-    #else:
-    #    os.system("./script/validatedataset.sh \"{0}\" {1}".format(os.path.join("..", opts.dataroot), '+'.join(opts.use_data)))
+    if opts.use_network == "abd":
+        #if not opts.use_resnet:
+        os.system("./script/testdatasetabd.sh \"{0}\" {1} {2}".format(os.path.join("..", opts.dataroot), '+'.join(opts.use_data), str(opts.sample_size)))
+        #else:
+            #os.system("./script/validatedataset.sh \"{0}\" {1}".format(os.path.join("..", opts.dataroot), '+'.join(opts.use_data)))
+    elif opts.use_network == "pose":
+        os.system("./script/testdatasetdg.sh")
