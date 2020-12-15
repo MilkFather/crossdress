3ebd6c7519f6af502f873ac27c492a5daee3f344
MilkFather <31627231+MilkFather@users.noreply.github.com>
2020-12-15 13:33:03 +0800

    Update testdataset.py

--- net/testdataset.py
+++ net/testdataset.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         os.system("./script/testdatasetabd.sh \"{0}\" {1} {2}".format(os.path.join("..", opts.dataroot), '+'.join(opts.use_data), str(opts.sample_size)))
         #else:
             #os.system("./script/validatedataset.sh \"{0}\" {1}".format(os.path.join("..", opts.dataroot), '+'.join(opts.use_data)))
-    elif opts.use_network == "pose":
+    elif opts.use_network == "dg":
         os.system("./script/testdatasetdg.sh")
