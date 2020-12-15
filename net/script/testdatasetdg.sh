4b3dcefd83a9cf8bfc2cfb8045b388a0fb06505d
MilkFather <31627231+MilkFather@users.noreply.github.com>
2020-12-15 13:04:58 +0800

    update configs

--- net/script/testdatasetdg.sh
+++ net/script/testdatasetdg.sh
@@ -7,7 +7,7 @@ source dressenv/bin/activate
 cd DG-Net
 python3 train.py --config configs/latest.yaml
 cd reid_eval
-python3 test_2label.py --name E0.5new_reid0.5_w30000  --which_epoch 100000 --multi
+python3 test_2label.py --name latest  --which_epoch 100000 --multi
 cd ..
 cd ..
 deactivate
